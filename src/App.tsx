67771e190648dff7e97f9847a77b377ce45d3f54
mfmurray <mfmurray@umich.edu>
2023-08-30 15:43:09 -0400

    feat: convert a signed in firebase user into a rownd user

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Todo from './Todo';
 import './App.css';
 import { useRownd } from '@rownd/react';
+import { auth } from './firebase';
 
 function App() {
-  const { is_authenticated, is_initializing, requestSignIn, user, signOut } = useRownd();
+  const { is_authenticated, is_initializing, requestSignIn, user, signOut, getAccessToken } = useRownd();
+  const [isSignedInFirebaseUser, setIsSignedInFirebaseUser] = useState<boolean | undefined>(undefined);
 
   useEffect(() => {
-    if (!is_authenticated && !is_initializing) {
+    const listener = auth.onAuthStateChanged((user) => {
+        setIsSignedInFirebaseUser(!!user);
+    });
+
+    return () => {
+      listener();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!is_authenticated && !is_initializing && typeof isSignedInFirebaseUser === 'boolean') {
+        // Convert an already signed in Firebase user into a Rownd user.
+        if (isSignedInFirebaseUser) {
+            auth.currentUser?.getIdToken().then((idToken) => {
+                getAccessToken({ token: idToken })
+            })
+            return;
+        }
+
         requestSignIn({ 
             prevent_closing: true,
         })
     }
-  }, [is_authenticated, is_initializing, requestSignIn])
+  }, [is_authenticated, is_initializing, requestSignIn, getAccessToken, isSignedInFirebaseUser]);
 
   if (is_initializing) {
     return <h2>Loading...</h2>
